3b4109b38f57b711d6c06568e43de9e8e899539f
jokepinocchio <jokepinocchio@gmail.com>
2016-01-10 16:22:28 +0800

    add comment

--- lisp/init-cscope.el
+++ lisp/init-cscope.el
@@ -1,4 +1,16 @@
-;;; setup xcscope.el from cscope src to site-lisp
+;;;; cscope - see `http://cscope.sourceforge.net/'
+;;;
+;;; Build and Install
+;;; ================
+;;; ./configure
+;;; make
+;;; sudo make install
+;;;
+;;; cp contrib/xcscope/xcscope.el ~/.emacs.d/site-lisp/xcscope/xcscope.el
+;;; sudo cp contrib/xcscope/cscope-indexer /usr/local/bin
+;;; sudo chmod 744 /usr/local/bin/cscope-indexer
+;;;
+;;; not require install xcscope in package, instead it use cscope src xcscope.el
 ;;(require-package 'xcscope)
 
 ;; only use xcscope in c/c++ mode
