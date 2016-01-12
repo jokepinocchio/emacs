5194fb896390debaab56ce612376d471c2b6b16b
caolei <caolei@pset.suntec.net>
2016-01-12 12:17:22 +0800

    add C-z to undo
    Change-Id: I0a8dd69c9dd1dd329441edb88cc6b46840adf25d

--- lisp/init-undo-tree.el
+++ lisp/init-undo-tree.el
@@ -1,6 +1,7 @@
 (require-package 'undo-tree)
 ;; (require 'undo-tree-autoloads)
-(global-set-key "\C-xu" 'undo-tree-visualize)
+(global-set-key (kbd "C-x u") 'undo-tree-visualize)
 (global-undo-tree-mode t)
+(global-set-key (kbd "C-z") 'undo)
 
 (provide 'init-undo-tree)
