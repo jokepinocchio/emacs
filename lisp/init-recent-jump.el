a1984fa20abf77adf6d15267bd096b8e130adb75
jokepinocchio <jokepinocchio@gmail.com>
2016-01-28 23:42:42 +0800

    Fix key

--- lisp/init-recent-jump.el
+++ lisp/init-recent-jump.el
@@ -2,7 +2,7 @@
 (setq recent-jump-threshold 20)
 (setq recent-jump-ring-length 100)
 (global-set-key (kbd "C-o") 'recent-jump-jump-backward)
-(global-set-key (kbd "C-i") 'recent-jump-jump-forward)
+(global-set-key (kbd "M-o") 'recent-jump-jump-forward)
 
 ;;(require 'quick-jump)
 ;;(quick-jump-default-keybinding)
