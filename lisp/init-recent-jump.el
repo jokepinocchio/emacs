e39310de0647e9eeaed59fc2ccd39d33e2e2fe56
jokepinocchio <jokepinocchio@gmail.com>
2016-01-28 22:45:38 +0800

    modify key map

--- lisp/init-recent-jump.el
+++ lisp/init-recent-jump.el
@@ -1,8 +1,8 @@
 (require 'recent-jump)
-(setq recent-jump-threshold 4)
-(setq recent-jump-ring-length 10)
+(setq recent-jump-threshold 20)
+(setq recent-jump-ring-length 100)
 (global-set-key (kbd "C-o") 'recent-jump-jump-backward)
-(global-set-key (kbd "M-o") 'recent-jump-jump-forward)
+(global-set-key (kbd "C-i") 'recent-jump-jump-forward)
 
 ;;(require 'quick-jump)
 ;;(quick-jump-default-keybinding)
