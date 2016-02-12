280582036751846a2efda5d0cfee92255221d32f
jokepinocchio <jokepinocchio@gmail.com>
2016-02-12 21:43:33 +0800

    Modify tabbar shortcuts

--- lisp/init-tabbar.el
+++ lisp/init-tabbar.el
@@ -5,7 +5,7 @@
 ;;(global-set-key (kbd "C-c p") 'tabbar-backward)
 ;;(global-set-key (kbd "C-c n") 'tabbar-forward)
 
-(global-set-key (kbd "M-j") 'tabbar-forward)
-(global-set-key (kbd "M-k") 'tabbar-backward)
+(global-set-key (kbd "M-J") 'tabbar-forward)
+(global-set-key (kbd "M-K") 'tabbar-backward)
 
 (provide 'init-tabbar)
